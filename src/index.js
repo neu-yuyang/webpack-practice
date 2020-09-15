18648bad05be107c890e84c65a5b23b7080335d0
uzumaki naruto <neu_yuyang@163.com>
2020-09-15 11:10:19 +0800

    添加prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import "./assets/style.css"
+import './assets/style.css'
 const bar = {
   a: {
     b: 123,
     c: {
-      d: "hello",
+      d: 'hello',
       e() {
         console.info(123)
       }
@@ -14,7 +14,7 @@ const bar = {
 const bb = {
   ...bar,
   app: [1, 2, 3, 4],
-  bpp: "hello world".includes("ll")
+  bpp: 'hello world'.includes('ll')
 }
 
 document.body.innerText = `这个是 ${JSON.stringify(bb)}`

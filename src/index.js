06a174483e07c46281b02597f016442c8c5109aa
uzumaki naruto <neu_yuyang@163.com>
2020-09-15 10:40:23 +0800

    添加babel配置

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import "./assets/style.css"
-function foo() {
-  document.body.innerText = "hello world"
+const bar = {
+  a: {
+    b: 123,
+    c: {
+      d: "hello",
+      e() {
+        console.info(123)
+      }
+    }
+  }
 }
-foo()
+
+const bb = {
+  ...bar,
+  app: [1, 2, 3, 4],
+  bpp: "hello world".includes("ll")
+}
+
+document.body.innerText = `这个是 ${JSON.stringify(bb)}`

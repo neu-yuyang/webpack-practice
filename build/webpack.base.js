eba1f4c13d89f12505d2fd30ceba161a92780ee8
uzumaki naruto <neu_yuyang@163.com>
2020-09-15 14:10:58 +0800

    添加postcss

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,24 +1,35 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = {
   entry: {
-    app: "./src/index.js"
+    app: './src/index.js'
   },
   output: {
-    filename: "[name].[hash].js",
-    path: path.resolve(__dirname, "../dist")
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, '../dist')
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
       }
     ]
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,37 +1,37 @@
-const path = require("path")
-const { merge } = require("webpack-merge")
+const path = require('path')
+const { merge } = require('webpack-merge')
 // 生成gzip文件
-const CompressionWebpackPlugin = require("compression-webpack-plugin")
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 // 分析打包出来文件的大小
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 // 将css提出为独立文件的插件
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 // 生成html5，并在body中使用script标签引入打包后的js文件
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 // 清理dist文件夹
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 // 压缩器，用于替代webpack自带的压缩器
-const TerserJSPlugin = require("terser-webpack-plugin")
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const TerserJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
-const base = require("./webpack.base")
+const base = require('./webpack.base')
 const config = {
   bundleAnalyzerReport: false,
   productionGzip: true
 }
 const webpackConfig = merge(base, {
-  mode: "production",
+  mode: 'production',
   output: {
-    filename: "[name].[chunkhash].js",
-    path: path.resolve(__dirname, "../dist")
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, '../dist')
   },
   // source-map: 整个source map 作为独立文件生成，并未bundle添加一个引用注释。
-  devtool: "source-map",
+  devtool: 'source-map',
   optimization: {
     minimizer: [new TerserJSPlugin(), new OptimizeCSSAssetsPlugin()],
     splitChunks: {
-      chunks: "all"
+      chunks: 'all'
     }
   },
   module: {
@@ -45,7 +45,15 @@ const webpackConfig = merge(base, {
               esModule: true
             }
           },
-          "css-loader"
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
         ]
       }
     ]
@@ -53,11 +61,11 @@ const webpackConfig = merge(base, {
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "[name].[chunkhash].css"
+      filename: '[name].[chunkhash].css'
     }),
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "index.html",
+      filename: 'index.html',
+      template: 'index.html',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -71,7 +79,7 @@ const webpackConfig = merge(base, {
 if (config.productionGzip) {
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
-      test: new RegExp("\\.(js|css)$"),
+      test: new RegExp('\\.(js|css)$'),
       threshold: 10240,
       minRatio: 0.8
     })

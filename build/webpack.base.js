85668d8d7b9ea0e43496265be7b09f163766e770
uzumaki naruto <neu_yuyang@163.com>
2020-09-18 14:26:07 +0800

    添加babel

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -13,6 +13,13 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       }
     ]
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./assets/style.css"
 function foo() {
-  document.body.innerText = "hello world"
+  let obj = {
+    age: 11
+  }
+  let objBak = { name: "yy", ...obj } // 不支持es6+语法的浏览器，会报错
+  document.body.innerText = JSON.stringify(objBak)
 }
 foo()

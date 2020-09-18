0d49055ebda44eae0b82bbf28eef4659cacf92d4
uzumaki naruto <neu_yuyang@163.com>
2020-09-18 17:26:42 +0800

    添加eslint-loader

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -15,6 +15,18 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          cache: true,
+          fix: false // 自动修复，对 ide 不够友好。如果，webstorm也开启了自动修复，会与eslint-loader互相冲突。
+          // failOnError: true, // 如果有格式错误，不进行编译
+          // failOnWarning: true
+        }
+      },
+      {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import "./assets/style.css"
+
 function foo() {
   const obj = {
     age: 11
   }
 
-  const objBak = { name: "yy", ...obj } // 不支持es6+语法的浏览器，会报错
+  const objBak = {
+    name: "yy",
+    ...obj
+  } // 不支持es6+语法的浏览器，会报错
   document.body.innerText = JSON.stringify(objBak)
 }
 foo()
